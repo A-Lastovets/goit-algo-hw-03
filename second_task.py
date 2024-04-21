898319429196ff0a93e77a765fee1316cf84f80a
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2024-04-21 12:50:06 +0200

    add comments(task is ready)

--- second_task.py
+++ second_task.py
@@ -1,11 +1,14 @@
 #Друге завдання
-from random import randint
+from random import randint                                       #Імпортуємо модуль для генерації випадкових чисел
 
-result_values = set()
+result_values = set()                                            #Створюємо множину, яка має назву result_values
 
-def get_numbers_ticket(min_value, max_value, quantity):
-    while len(result_values) != quantity:
-        result_values.add(randint(min_value, max_value))
-    print("Ваші лотерейні числа:", sorted(list(result_values)))
+def get_numbers_ticket(min_value, max_value, quantity):          #Створюємо функцію
+    while len(result_values) != quantity:                        #Створюємо цикл для генерації чисел поки лічильник не перевищіть число 6 (бо 6 чисел нам потрібно, не більше)
+        result_values.add(randint(min_value, max_value))         #Додаємо рандомно числа від 1 до 49
+    print("Ваші лотерейні числа:", sorted(list(result_values)))  #Друкуємо відсортований список
 
-get_numbers_ticket(1, 49, 6)
+get_numbers_ticket(1, 49, 6)                                     #Викливаємо функцію з параметрами
+
+#Результат:
+# Ваші лотерейні числа: [6, 15, 31, 35, 37, 39]
