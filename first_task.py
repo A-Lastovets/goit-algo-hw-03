85ec97bb26c953ff1b32a689ee6024a3a8e007ab
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2024-04-22 00:30:55 +0200

    Task_after_modification

--- first_task.py
+++ first_task.py
@@ -3,24 +3,13 @@
 from datetime import datetime, timedelta
 
 def get_days_from_today(date):
-
-    specific_date = datetime(year=2020, month=4, day=21).date()                    #створення об'єкта datetime з певною датою
-    print(specific_date, " - створення об'єкта datetime з певною датою")           #для себе перевіряю яку дату показує
-
+    specific_date = datetime(year=2023, month=9, day=28).date()                    #створення об'єкта datetime з певною датою
     current_date = datetime.today().date()                                         #отримання поточної дати
-    print(current_date, " - отримання поточної дати")                              #для себе перевіряю яку дату показує
-    
     different_days = (current_date - specific_date).days                           #знаходимо різницю між двома датами
-    print("Різниця між датами, число: ", different_days)                           #вивести різницю (число) між датами
-    print("Тип надрукованого числа: ", type(different_days))                       #для себе перевіряю який тип надрукованого числа показує
+    print("Різниця між датами, число: ", different_days)                           #вивести різницю (число) між датами                         
+    return different_days
 
 
 get_days_from_today(date="")                                                       #виклик функції
-
-#Результат:
-# 2020-04-21  - створення об'єкта datetime з певною датою
-# 2024-04-21  - отримання поточної дати
-# Різниця між датами, число:  1461
-# Тип надрукованого числа:  <class 'int'>
 
 
--- second_task.py
+++ second_task.py
@@ -1,14 +1,12 @@
 #Друге завдання
-from random import randint                                       #Імпортуємо модуль для генерації випадкових чисел
+from random import sample                                       #Імпортуємо модуль для генерації випадкових чисел
 
-result_values = set()                                            #Створюємо множину, яка має назву result_values
+def get_numbers_ticket(min_value, max_value, quantity):
+        if max_value > min_value > 0 and max_value <= 1000 and min_value != max_value and 0 < quantity < (max_value - min_value) + 1:
 
-def get_numbers_ticket(min_value, max_value, quantity):          #Створюємо функцію
-    while len(result_values) != quantity:                        #Створюємо цикл для генерації чисел поки лічильник не перевищіть число 6 (бо 6 чисел нам потрібно, не більше)
-        result_values.add(randint(min_value, max_value))         #Додаємо рандомно числа від 1 до 49
-    print("Ваші лотерейні числа:", sorted(list(result_values)))  #Друкуємо відсортований список
+            print(sorted(sample(range(min_value, max_value+1), quantity)))
+        else:
+              print([])
 
-get_numbers_ticket(1, 49, 6)                                     #Викливаємо функцію з параметрами
 
-#Результат:
-# Ваші лотерейні числа: [6, 15, 31, 35, 37, 39]
+get_numbers_ticket(10, 15, 6)                                     #Викливаємо функцію з параметрами
