6f861730d5a896f278bd7e007f4ebe5ff3b46145
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2024-04-21 13:06:52 +0200

    third_task_ready

--- third task.py
+++ third task.py
@@ -1,11 +1,16 @@
 
 import re
 
-phone_numbers = ["    +38(050)123-32-34"
-"     0503451234"
-"(050)8889900"
-"38050-111-22-22"
-"38050 111 22 11   "]
+phone_numbers = ["+38(050)\\123-32-34", "050t3451234", "(O50)888990O", "38050-1I1-2p2-22", "38o50 111 22 11 jer"]
 
 def normalize_phone(phone_numbers):
-    
+    for phone in phone_numbers:
+        clear_number = re.sub(r'\D', '', phone)
+        if not clear_number.startswith('+'):
+            if clear_number.startswith('380'):
+                clear_number = '+' + clear_number
+            else:
+                clear_number = '+380' + clear_number
+        print(clear_number)
+
+normalize_phone(phone_numbers)
